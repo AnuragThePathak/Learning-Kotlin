f46c88da951805a680111ccfba53a9d2610221e4
Anurag Pathak <anuragpathak911@gmail.com>
2021-09-24 10:58:00 +0530

    String conversion.

---
+++ src/main/kotlin/strings/ImplicitConversionToString.kt
@@ -0,0 +1,21 @@
+package strings
+
+class ImplicitConversionToString {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello" + 9)
+            println("Hello" + 'a')
+            println("Hello" + 9 + 'a')
+//            println(9 + "Hello") // Unlike java, kotlin don't convert to String
+//            if number appears in the beginning
+            println('a' + "Hello")
+
+//            println(9 + 'a' + "Hello")
+            println('a' + 9 + "Hello")
+
+//            println(9 + 'a')
+            println('a' + 9)
+        }
+    }
+}
